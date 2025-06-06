37f37f5ffa925748d3e7c1201ce9e888127ac4b5
liam-brha-rmit <s4090676@student.rmit.edu.au>
2025-06-06 15:16:14 +1000

    first cut at removing original localstorage hooks

--- apps/web-user/src/hooks/localstorage/useUser.tsx
+++ apps/web-user/src/hooks/localstorage/useUser.tsx
@@ -1,39 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useStore } from "@/hooks/localstorage/useStore"
 import { UUID } from "@/context/localstorage/types"
-import { AccountDetails } from "@repo/database/types/AccountDetails"
-import { useAction } from "@/hooks/api/useApi"
-import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { AccountType } from "@repo/types/enums"
 
-/**
- * Get information on the currently authenticated user
- * Or update some part of the record
- * Or pass an id and type to look up a user
- */
-export function useUser<T extends AccountDetails, SignedIn = false>(
-  userId?: UUID
-): [
-  typeof query,
-  SignedIn extends true ? typeof updateUser : typeof updateUser | false,
-] {
-  const [loggedInUserId] = useStore("userId")
-  const queryClient = useQueryClient()
+export function useUser(): [UUID, AccountType] {
+  const uId =  localStorage.getItem("userId")
+  const type =  localStorage.getItem("userType")
+  if (!uId || !type) throw new Error("No userId and type found in localStorage")
+  return [uId, type as AccountType]
+}
 
-  const targetId = loggedInUserId || userId
-
-  const query = useQuery<T>({
-    enabled: !!targetId,
-    queryKey: ["/user", targetId],
-  })
-
-  const updateUser: (body: Partial<AccountDetails>) => Promise<AccountDetails> =
-    useAction<AccountDetails>({
-      path: `/user/${userId}`,
-      options: { method: "PUT" },
-      onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ["user", targetId] })
-      },
-    })
-
-  return [query, !!targetId && (updateUser as any)]
+export function setUser(userId: UUID, accountType: AccountType): void {
+  localStorage.setItem("userId", JSON.stringify(userId))
+  localStorage.setItem("userType", JSON.stringify(accountType))
 }
