b139d1e6a790b30f6e9b0da29a415d64963be826
liam-brha-rmit <s4090676@student.rmit.edu.au>
2025-06-08 15:46:02 +1000

    git ignore code gen file

--- apps/server-admin/codegen.ts
+++ apps/server-admin/codegen.ts
@@ -3,7 +3,6 @@ import type { CodegenConfig } from "@graphql-codegen/cli"
 const config: CodegenConfig = {
   overwrite: true,
   schema: "Schema.graphql",
-  watch: true,
   emitLegacyCommonJSImports: false,
   generates: {
     "src/__generated__/resolvers-types.ts": {
